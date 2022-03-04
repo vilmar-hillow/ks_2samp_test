bba20a0ee7f563ee3a10f147ff00287666e60193
Dmitry Supranovich <DSupranovich@munichre.ca>
2022-03-04 20:36:33 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ks_2samp_test
+# A reproducible example of the behaviour of `scipy.stats.ks_2samp`
+
+## Dependencies
+
+The script's dependencies can be installed by running:
+
+    pip install pandas scikit-learn scipy
+
+Or, using `pipenv`:
+
+    pipenv install --ignore-pipfile
+
+    pipenv shell
+
+## Running the script
+
+    python ks_2samp_test.py
---
+++ ks_2samp_test.py
@@ -0,0 +1,45 @@
+"""
+Test ks_2samp behaviour.
+"""
+
+import pandas as pd
+from scipy.stats import ks_2samp
+from sklearn.preprocessing import MinMaxScaler
+
+
+def get_data():
+    """Fetch iris dataset."""
+    names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    data = pd.read_csv(url, names=names)
+
+    return data
+
+
+def get_features(data: pd.DataFrame):
+    """Generate features."""
+    data = data.copy()
+
+    numeric_fields = [
+        "sepal-length",
+        "sepal-width",
+        "petal-length",
+        "petal-width",
+    ]
+
+    data_numeric = data[numeric_fields]
+    scaler = MinMaxScaler()
+    scaler.fit(data_numeric)
+    data[numeric_fields] = scaler.transform(data_numeric)
+
+    return data[numeric_fields]
+
+if __name__ == "__main__":
+    raw_data = get_data()
+    features = get_features(raw_data)
+    features.to_csv("reference.csv", index=False)
+    reference = pd.read_csv("reference.csv")
+
+    for feature_name in features.columns:
+        scipy_p_value = ks_2samp(reference[feature_name], features[feature_name])[1]
+        print(f"Feature '{feature_name}' p value: {scipy_p_value}")
